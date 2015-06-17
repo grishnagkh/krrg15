169ea909282a6c86f4553db2469a1fc5a7f39f46
Verena Dittmer <verena_dittmer@web.de>
2015-06-17 15:26:53 +0200

    pieceCount eval

--- Minimax.java
+++ Minimax.java
@@ -1,199 +1,301 @@
 import java.util.HashMap;
 
 public class Minimax implements ISolver{
-        private int cutoffdepth, transTblCutoffDepth;
-        public int playerID, opponentID;
-        private boolean returnTwice = false;
-        private HashMap<State, Integer> transTbl = new HashMap<State, Integer>(100000);
-        private static int hitCounter = 0, count = 0;
-        private GameLogic gameLogic;
-        private enum CutoffActions {finalState, cutoff, playOn}
-        private final int winSymbol = Integer.MAX_VALUE - 1;
-        private final int lossSymbol = Integer.MIN_VALUE + 1;
-        private boolean firstMove = true;
-        
-        public Minimax(int playerID, int opponentID, GameLogic gameLogic, int d){
-                this.playerID = playerID;
-                this.opponentID = opponentID;
-                this.gameLogic = gameLogic;
+  private int cutoffdepth, transTblCutoffDepth;
+  public int playerID, opponentID;
+  private boolean returnTwice = false;
+  private HashMap<State, Integer> transTbl = new HashMap<State, Integer>(100000);
+  private static int hitCounter = 0, count = 0;
+  private GameLogic gameLogic;
+  private enum CutoffActions {finalState, cutoff, playOn}
+  private final int winSymbol = Integer.MAX_VALUE - 1;
+  private final int lossSymbol = Integer.MIN_VALUE + 1;
+  private boolean firstMove = true;
+  private Seq seq;
+  private int quadLength;
+  private int excessRows;
+  private int excessCols;
+
+  public Minimax(int playerID, int opponentID, GameLogic gameLogic, int d){
+    this.playerID = playerID;
+    this.opponentID = opponentID;
+    this.gameLogic = gameLogic;
 //                seq = new Seq(playerID, opponentID);
-                this.cutoffdepth = d;
-                this.transTblCutoffDepth = d;
-        }
-        
-        public int getDecision(State s){
-                
-                if (firstMove) {
-                        firstMove = false;
-                        return s.gameBoard.length / 2 ;
-                }
-                
-                long startTime = System.currentTimeMillis();
-                int pick = Integer.MIN_VALUE; 
-                int v = Integer.MIN_VALUE;
-                for (Integer column : s.openCols){
-                        State sPrime = new State(s);
-                        sPrime.insertCoin(column, playerID);
-                        int val;
+    quadLength = Math.min(gameLogic.board.gameBoard.length, gameLogic.board.gameBoard[0].length);
+    excessRows = Math.max(0, gameLogic.board.gameBoard[0].length - gameLogic.board.gameBoard.length);
+    excessCols = Math.max(0, gameLogic.board.gameBoard.length - gameLogic.board.gameBoard[0].length);
+    this.cutoffdepth = d;
+    this.transTblCutoffDepth = d;
+  }
+
+  public int getDecision(State s){
+
+    if (firstMove) {
+      firstMove = false;
+      return s.gameBoard.length / 2 ;
+    }
+
+    long startTime = System.currentTimeMillis();
+    int pick = Integer.MIN_VALUE;
+    int v = Integer.MIN_VALUE;
+    for (Integer column : s.openCols){
+      State sPrime = new State(s);
+      sPrime.insertCoin(column, playerID);
+      int val;
 //                      val = MinValue(sPrime, Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
 //                      System.out.println("col: " + column + " val: "+val);
-                        if (transTbl.containsKey(sPrime)) {
-                                val = transTbl.get(sPrime);
-                                hitCounter++;
-                        } else {
-                                val = MinValue(sPrime, Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
-                                if (val == winSymbol || val == lossSymbol)transTbl.put(sPrime, val);
-                        }
-                        
-                        if (val > v){
-                                pick = column;
-                                if (val == winSymbol) break;
-                                v = val;
-                        }
-                }
-                long endTime = System.currentTimeMillis();
-                long timeTaken = (endTime - startTime) / 1000;
-                if (timeTaken < 6) {
-                        cutoffdepth++;
-                } 
-                System.out.println("time: " + timeTaken);
-                System.out.println("cutoff: " + cutoffdepth);
-                System.out.println("hits: " + hitCounter);
-                System.out.println("count: " + count);
-                System.out.println("tvlSize: " + transTbl.size() + "\n");
-                count = hitCounter = 0;
-                return pick;
-        }
-        
-        public int MaxValue(State s, int alpha, int beta, int depth){
-                count++;
+      if (transTbl.containsKey(sPrime)) {
+        val = transTbl.get(sPrime);
+        hitCounter++;
+      } else {
+        val = MinValue(sPrime, Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
+        if (val == winSymbol || val == lossSymbol)transTbl.put(sPrime, val);
+      }
 
-                CutoffActions action = cutoffTest(s, depth);
-                if (action != CutoffActions.playOn){
-                        if (action == CutoffActions.finalState){
-                                int utility = gameLogic.TerminalTest(s);
-                                if (utility == playerID) return winSymbol;
-                                else if (utility == opponentID) return lossSymbol;
-                                else return -1000;
-                        }
-                        else if (action == CutoffActions.cutoff) return eval(s);
-                }
-                
-                int v = Integer.MIN_VALUE;
-                for (Integer column : s.openCols){
-                        State sPrime = new State(s);
-                        sPrime.insertCoin(column, playerID);
-                        int val;
+      if (val > v){
+        pick = column;
+        if (val == winSymbol) break;
+        v = val;
+      }
+    }
+    long endTime = System.currentTimeMillis();
+    long timeTaken = (endTime - startTime) / 1000;
+    if (timeTaken < 6) {
+      cutoffdepth++;
+    }
+    System.out.println("time: " + timeTaken);
+    System.out.println("cutoff: " + cutoffdepth);
+    System.out.println("hits: " + hitCounter);
+    System.out.println("count: " + count);
+    System.out.println("tvlSize: " + transTbl.size() + "\n");
+    count = hitCounter = 0;
+    return pick;
+  }
+
+  public int MaxValue(State s, int alpha, int beta, int depth){
+    count++;
+
+    CutoffActions action = cutoffTest(s, depth);
+    if (action != CutoffActions.playOn){
+      if (action == CutoffActions.finalState){
+        int utility = gameLogic.TerminalTest(s);
+        if (utility == playerID) return winSymbol;
+        else if (utility == opponentID) return lossSymbol;
+        else return -1000;
+      }
+      else if (action == CutoffActions.cutoff) return eval(s);
+    }
+
+    int v = Integer.MIN_VALUE;
+    for (Integer column : s.openCols){
+      State sPrime = new State(s);
+      sPrime.insertCoin(column, playerID);
+      int val;
 //                      val = MinValue(sPrime, alpha, beta, depth+1);
-                        if (transTbl.containsKey(sPrime)) {
-                                val = transTbl.get(sPrime);
-                                hitCounter++;
-                        } else {
-                                val = MinValue(sPrime, alpha, beta, depth+1);
-                                if (depth < transTblCutoffDepth && (val == winSymbol || val == lossSymbol) ) transTbl.put(sPrime, val);
-                                }
-                        
-                        if (val > v){
-                                if (val == winSymbol) return val;
-                                v = val;
-                        }
-                        if (v >= beta) return v;
-                        if (v > alpha) alpha = v;
-                }
-                return v;
-        }
-        
-        public int MinValue(State s, int alpha, int beta, int depth){
-                count++;
-                CutoffActions action = cutoffTest(s, depth);
-                if (action != CutoffActions.playOn){
-                        if (action == CutoffActions.finalState){
-                                int utility = gameLogic.TerminalTest(s);
-                                if (utility == playerID) return winSymbol;
-                                else if (utility == opponentID) return lossSymbol;
-                                else return -1000  ;
-                        }
-                        else if (action == CutoffActions.cutoff) return eval(s);
-                }
+      if (transTbl.containsKey(sPrime)) {
+        val = transTbl.get(sPrime);
+        hitCounter++;
+      } else {
+        val = MinValue(sPrime, alpha, beta, depth+1);
+        if (depth < transTblCutoffDepth && (val == winSymbol || val == lossSymbol) ) transTbl.put(sPrime, val);
+      }
 
-                int v = Integer.MAX_VALUE;
-                for (Integer column : s.openCols){
-                        State sPrime = new State(s);
-                        sPrime.insertCoin(column, opponentID);
-                        int val;
+      if (val > v){
+        if (val == winSymbol) return val;
+        v = val;
+      }
+      if (v >= beta) return v;
+      if (v > alpha) alpha = v;
+    }
+    return v;
+  }
+
+  public int MinValue(State s, int alpha, int beta, int depth){
+    count++;
+    CutoffActions action = cutoffTest(s, depth);
+    if (action != CutoffActions.playOn){
+      if (action == CutoffActions.finalState){
+        int utility = gameLogic.TerminalTest(s);
+        if (utility == playerID) return winSymbol;
+        else if (utility == opponentID) return lossSymbol;
+        else return -1000  ;
+      }
+      else if (action == CutoffActions.cutoff) return eval(s);
+    }
+
+    int v = Integer.MAX_VALUE;
+    for (Integer column : s.openCols){
+      State sPrime = new State(s);
+      sPrime.insertCoin(column, opponentID);
+      int val;
 //                      val = MaxValue(sPrime, alpha, beta, depth+1);
-                        if (transTbl.containsKey(sPrime)) {
-                                val = transTbl.get(sPrime);
-                                hitCounter++;
-                        } else {
-                                val = MaxValue(sPrime, alpha, beta, depth+1);
-                                if (depth < transTblCutoffDepth && (val == winSymbol || val == lossSymbol) ) transTbl.put(sPrime, val);
-                        }
-                        
-                        if (returnTwice){
-                                returnTwice = false;
-                                return val;
-                        }
-                        if (val < v){
-                                v = val;
-                        }
-                        if (v <= alpha) return v;
-                        if (v < beta) beta = v;
-                }
-                return v;
+      if (transTbl.containsKey(sPrime)) {
+        val = transTbl.get(sPrime);
+        hitCounter++;
+      } else {
+        val = MaxValue(sPrime, alpha, beta, depth+1);
+        if (depth < transTblCutoffDepth && (val == winSymbol || val == lossSymbol) ) transTbl.put(sPrime, val);
+      }
+
+      if (returnTwice){
+        returnTwice = false;
+        return val;
+      }
+      if (val < v){
+        v = val;
+      }
+      if (v <= alpha) return v;
+      if (v < beta) beta = v;
+    }
+    return v;
+  }
+
+  private CutoffActions cutoffTest(State state, int depth){
+    if (gameLogic.TerminalTest(state) != 0){
+      return CutoffActions.finalState;
+    }
+    if (depth >= cutoffdepth){
+      return CutoffActions.cutoff;
+    }
+    return CutoffActions.playOn;
+  }
+
+  private int eval(State s){
+    int eval = 0;
+
+    int[][] gameBoard = s.gameBoard;
+    int[][] gameBoardMirrored = mirror(s.gameBoard);
+
+    seq = new Seq(playerID, opponentID);
+
+    // vertical (columns)
+    for (int i = 0; i < gameBoard.length; i++) {
+      for (int j = 0; j < gameBoard[0].length; j++) {
+        int seqResult = seq.evalPiece(gameBoard[i][j]);
+
+        // terminal test
+        if (seqResult == Integer.MAX_VALUE || seqResult == Integer.MIN_VALUE) {
+          return seqResult;
         }
-        
-        private CutoffActions cutoffTest(State state, int depth){
-                if (gameLogic.TerminalTest(state) != 0){
-                        return CutoffActions.finalState;
-                }
-                if (depth >= cutoffdepth){
-                        return CutoffActions.cutoff;
-                }
-                return CutoffActions.playOn;
+        eval += seqResult;
+      }
+      eval += seq.reset();
+    }
+    eval += seq.reset();
+
+    // horizontal (rows)
+    for (int i = 0; i < gameBoard[0].length; i++) {
+      for (int j = 0; j < gameBoard.length; j++) {
+        int seqResult = seq.evalPiece(gameBoard[j][i]);
+
+        // terminal test
+        if (seqResult == Integer.MAX_VALUE || seqResult == Integer.MIN_VALUE) {
+          return seqResult;
         }
-        
-        private int eval(State s){
-          int eval = 0;
+        eval += seqResult;
+      }
+      eval += seq.reset();
+    }
+    eval += seq.reset();
 
-          final int MAX = 1;
-          final int MIN = 2;
-          int countingForPlayer = 0;
-          int currentCount = 0;
+    // diagonal (top left to bottom right)
 
-          int[][] gameBoard = s.gameBoard;
+    // iterate vertically / handle excessRows
+    int diagLength = 1;
+    for (int i = 0; i < quadLength + excessRows; i++) {
+      for (int j = 0; j < diagLength; j++) {
+        int seqResult = seq.evalPiece(gameBoard[j][i-j]);
 
-          // horizontal (rows)
-          for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard[0].length; j++) {
-
-              // max piece
-              if (gameBoard[i][j] == MAX) {
-                if (countingForPlayer == MAX) {
-                  currentCount++;
-                } else {
-
-                  // reset currentCount, set countingForPlayer == MIN and add to eval
-                  if (currentCount > 1) {
-                    eval += currentCount;
-
-                    //
-                  }
-                  currentCount = 1;
-
-                  countingForPlayer = MIN;
-                }
-              }
-
-              // min piece
-              if (gameBoard[i][j] == MIN) {
-                if (countingForPlayer == MIN) {
-                  currentCount++;
-                }
-              }
-            }
-          }
-
-          return eval;
+        // terminal test
+        if (seqResult == Integer.MAX_VALUE || seqResult == Integer.MIN_VALUE) {
+          return seqResult;
         }
+        eval += seqResult;
+      }
+      eval += seq.reset();
+
+      if (diagLength < quadLength) {
+        diagLength++;
+      }
+    }
+    eval += seq.reset();
+
+    // iterate horizontally / handle excessCols
+    diagLength = quadLength;
+    for (int i = 1; i < quadLength + excessCols; i++) {
+      for (int j = 0; j < diagLength; j++) {
+        int seqResult = seq.evalPiece(gameBoard[j + i][quadLength - 1 - j]);
+
+        // terminal test
+        if (seqResult == Integer.MAX_VALUE || seqResult == Integer.MIN_VALUE) {
+          return seqResult;
+        }
+        eval += seqResult;
+      }
+      eval += seq.reset();
+
+      // last triangle where diagLength is reducing to 1
+      if (i >= gameBoard[0].length - quadLength) {
+        diagLength--;
+      }
+    }
+    eval += seq.reset();
+
+    // diagonal (bottom left to top right) - use mirrored gameboard
+
+    // iterate vertically  / handle excessRows
+    diagLength = 1;
+    for (int i = 0; i < quadLength + excessRows; i++) {
+      for (int j = 0; j < diagLength; j++) {
+        int seqResult = seq.evalPiece(gameBoardMirrored[j][i-j]);
+
+        // terminal test
+        if (seqResult == Integer.MAX_VALUE || seqResult == Integer.MIN_VALUE) {
+          return seqResult;
+        }
+        eval += seqResult;
+      }
+      eval += seq.reset();
+
+      if (diagLength < quadLength) {
+        diagLength++;
+      }
+    }
+    eval += seq.reset();
+
+    // iterate horizontally / handle excessCols
+    diagLength = quadLength;
+    for (int i = 1; i < quadLength + excessCols; i++) {
+      for (int j = 0; j < diagLength; j++) {
+        int seqResult = seq.evalPiece(gameBoardMirrored[j + i][quadLength - 1 - j]);
+
+        // terminal test
+        if (seqResult == Integer.MAX_VALUE || seqResult == Integer.MIN_VALUE) {
+          return seqResult;
+        }
+        eval += seqResult;
+      }
+      eval += seq.reset();
+
+      // last triangle where diagLength is reducing to 1
+      if (i >= gameBoardMirrored[0].length - quadLength) {
+        diagLength--;
+      }
+    }
+    eval += seq.reset();
+
+    return eval;
+  }
+
+  private int[][] mirror(int[][] gameBoard) {
+    int[][] ret = new int[gameBoard.length][gameBoard[0].length];
+
+    for (int i = 0; i < gameBoard.length; i++) {
+      for (int j = 0; j < gameBoard[0].length; j++) {
+        ret[i][j] = gameBoard[gameBoard.length - 1 - i][j];
+      }
+    }
+
+    return ret;
+  }
 }
---
+++ Seq.java
@@ -0,0 +1,89 @@
+public class Seq {
+  private int playerID;
+  private int opponentID;
+  private int currentCount;
+  private int countingForPlayer;
+
+  public Seq(int playerID, int opponentID) {
+    this.playerID = playerID;
+    this.opponentID = opponentID;
+    this.currentCount = 0;
+  }
+
+  public int evalPiece(int piece) {
+    int ret = 0;
+
+    // no piece
+    if (piece == 0) {
+
+      // reset current count
+      if (currentCount > 1) {
+        ret = currentCount;
+      }
+
+      currentCount = 0;
+      countingForPlayer = 0;
+    }
+
+    // max piece
+    if (piece == playerID) {
+      if (countingForPlayer == playerID) {
+        currentCount++;
+
+        // for terminal test
+        if (currentCount >= 4) {
+          ret = Integer.MAX_VALUE;
+        }
+      } else {
+
+        // add current count to eval, set countingForPlayer == opponentID and reset currentCount
+        if (currentCount > 1) {
+          ret = currentCount;
+        }
+
+        currentCount = 1;
+        countingForPlayer = playerID;
+      }
+    }
+
+    // min piece
+    else if (piece == opponentID) {
+      if (countingForPlayer == opponentID) {
+        currentCount++;
+
+        // for terminal test
+        if (currentCount >= 4) {
+          ret = Integer.MIN_VALUE;
+        }
+      } else {
+
+        // add current count to eval, set countingForPlayer == playerID and reset currentCount
+        if (currentCount > 1) {
+          ret = -1 * currentCount;
+        }
+
+        currentCount = 1;
+        countingForPlayer = opponentID;
+      }
+    }
+
+    return ret;
+  }
+
+  private int evalLastPiece() {
+    if (currentCount > 1) {
+      if (countingForPlayer == playerID) {
+        return currentCount;
+      } else {
+        return -1 * currentCount;
+      }
+    }
+    return 0;
+  }
+
+  public int reset() {
+    int ret = evalLastPiece();
+    currentCount = 0;
+    return ret;
+  }
+}
