e3b5acfe1834f4f2c9d812097d58ad20e39ac902
Fabian <schoberf@gmx.net>
2015-06-22 15:48:59 +0200

    Basic node (untested!) // Visits, Value & Outcome yet undone

--- at/connect4/solver/mcts/Node.java
+++ at/connect4/solver/mcts/Node.java
@@ -1,9 +1,9 @@
 package at.connect4.solver.mcts;
 
-import java.util.ArrayList;
-
 import at.connect4.game.GameLogic;
 import at.connect4.game.State;
+
+import java.util.ArrayList;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,76 +12,114 @@ import at.connect4.game.State;
  */
 
 /**
- *
  * @author erich
  */
-public class Node implements Node_I{
+public class Node implements Node_I {
     public State state;
     public GameLogic logic;
-    
+
+    private ArrayList<Node_I> children;
+    private Node_I parent;
+    private Integer action;
+
+    private ArrayList<Integer> untriedActions;
+    private int visits;
+
     public Node(State state, GameLogic logic) {
         this.state = state;
         this.logic = logic;
+
+        this.untriedActions = new ArrayList<>(state.openCols);
+
+        this.visits = 0;
     }
-    
-    public void setParent(Node_I n){
+
+    public void setParent(Node_I n) {
+        this.parent = n;
+    }
+
+    public Node_I getParent() {
+        return parent;
+    }
+
+    public Object getAction() {
+        return action;
+    }
+
+    public void setAction(Object action) {
+        if (action instanceof Integer) {
+            this.action = (Integer) action;
+        }
+    }
+
+    public ArrayList<Node_I> getChildren() {
+        return children;
+    }
+
+    public Node_I createChild(Object action) {
+        try {
+            Integer chosenColumn = (Integer) action;
+            int currentPlayer = state.getCurrentPlayer();
+
+            State childState = new State(state);
+            childState.insertCoin(chosenColumn, currentPlayer);
+
+            GameLogic childLogic = new GameLogic();
+            childLogic.board = childState;
+            childLogic.playerID = this.logic.opponentID;
+            childLogic.opponentID = this.logic.playerID;
+
+            Node child = new Node(childState, childLogic);
+            child.setParent(this);
+            child.setAction(action);
+
+            untriedActions.remove(action);
+
+            return child;
+        } catch (ClassCastException cce) {
+            System.out.println("Action provided to create a child node is no Integer: " + action);
+            return null;
+        }
+    }
+
+    public void setChild(Node_I child) {
+        children.add(child);
+    }
+
+    public ArrayList<Object> getUntriedActions() {
+        return (ArrayList<Object>) (ArrayList<?>) untriedActions;
+    }
+
+    public boolean hasUntriedActions() {
+        return !untriedActions.isEmpty();
+    }
+
+    public boolean isTerminal() {
+        return logic.TerminalTest(state) != 0;
+    }
+
+    public double getValue() {
         throw new RuntimeException("to be implemented!");
     }
-    
-    public Node_I getParent(){
+
+    public void setValue(double value) {
         throw new RuntimeException("to be implemented!");
     }
-    
-    public Object getAction(){
+
+    public int getVisits() {
+        return visits;
+    }
+
+    public void setVisits(int num) {
         throw new RuntimeException("to be implemented!");
     }
-    
-    public ArrayList<Node_I> getChildren(){
+
+    public double getReward(double outcome) {
         throw new RuntimeException("to be implemented!");
     }
-    
-    public Node_I createChild(Object action){
+
+    public double getOutcome() {
         throw new RuntimeException("to be implemented!");
     }
-    
-    public void setChild(Node_I n){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public ArrayList<Object> getUntriedActions(){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public boolean hasUntriedActions(){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public boolean isTerminal(){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public double getValue(){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public void setValue(double value){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public int getVisits(){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public void setVisits(int num){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public double getReward(double outcome){
-        throw new RuntimeException("to be implemented!");
-    }
-    
-    public double getOutcome(){
-        throw new RuntimeException("to be implemented!");
-    }
-    
+
 }
