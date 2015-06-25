7cd97263d34dffb71aefe529c13fcb822bdc1c7a
Stefan Petscharnig <stepetsc@gmail.com>
2015-06-25 17:30:25 +0200

    added evaluation functionality & results... 6.txt is cumulative result file

--- at/connect4/game/ShowGame.java
+++ at/connect4/game/ShowGame.java
@@ -8,118 +8,175 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import java.io.*;
 
 public class ShowGame {
+	static final int cols = 7;// 7;
+	static final int rows = 6;// 6;
 
-	public static void main(String[] arg) {
-		final int cols = 7;// 7;
-		final int rows = 6;// 6;
-
-		final GameLogic player1 = new GameLogic();
-		final GameLogic player2 = new GameLogic();
-
-		player1.initializeGame(cols, rows, 1);
-		player2.initializeGame(cols, rows, -1);
-
-
-		Evaluator pc1 = new at.connect4.eval.PieceCounter(player1);
-		Evaluator pc2 = new at.connect4.eval.PieceCounter(player1);
-		Evaluator mc1 = new at.connect4.eval.MCEval(player1);
-		Evaluator mc2 = new at.connect4.eval.MCEval(player2);
-
-		int nruns1, nruns2;
-
-		nruns1= 100;
-		//nruns1= 100;
-		//nruns1= 150;
-		//nruns1= 200;
-		//nruns2 = 50;
-		nruns2 = 200;
-		//nruns2 = 150;
-//		nruns2 = 200;
-
-		((at.connect4.eval.MCEval) mc1).setRuns(nruns1);
-		((at.connect4.eval.MCEval) mc2).setRuns(nruns2);
-
-		int depth1, depth2;
-
-//		depth1 = 3;
-//		depth2 = 3;
-		depth1 = 4;
-		depth2 = 4;
-	//	depth1 = 5;
-	//	depth2 = 5;
-//		depth1 = 6;
-//		depth2 = 6;
-
-
-		ISolver pcsolver1 = new at.connect4.solver.Negamax(player1.getPlayerID(), player1, depth1, pc1);
-		ISolver pcsolver2 = new at.connect4.solver.Negamax(player2.getPlayerID(), player2, depth2, pc2);
-		ISolver mcsolver1 = new at.connect4.solver.Negamax(player1.getPlayerID(), player1, depth1, mc1);
-		ISolver mcsolver2 = new at.connect4.solver.Negamax(player2.getPlayerID(), player2, depth2, mc2);
-
-
-		int time;
-		time = 1;
-		time = 2;
-		time = 3;
-
-		ISolver uct1 = new Uct4connect(player1.playerID, player1.opponentID, player1, time);
-		ISolver uct2 = new Uct4connect(player2.playerID, player2.opponentID, player1, time);
-
-		player1.setSolver(pcsolver1);
-		player2.setSolver(pcsolver2);
-//		player1.setSolver(mcsolver1);
-//		player2.setSolver(mcsolver2);
-//		player1.setSolver(uct1);
-//		player2.setSolver(uct2);
-
-
-//		player1.setSolver(new at.connect4.solver.Negamax(player1.getPlayerID(),
-//				player1, 6, new at.connect4.eval.PieceCounter(player1)));
-//		player2.setSolver(new at.connect4.solver.Negamax(player2.getPlayerID(),
-//				player2, 6, new at.connect4.eval.PieceCounter(player2)));
-
-		
-		//player1.setSolver(new at.connect4.solver.Minimax(player1.getPlayerID(),
-		// player1.getOpponentID(), player1, 3, new at.connect4.eval.PieceCounter(player1)));
-		// player2.setSolver(new at.connect4.solver.Minimax(player2.getPlayerID(),
-		// player2.getOpponentID(), player2, 3, new at.connect4.eval.PieceCounter(player2)));
-
-        //player1.setSolver(new Uct4connect(player1.playerID, player1.opponentID, player1, 1));
-        //player2.setSolver(new Uct4connect(player2.playerID, player2.opponentID, player2, 1));
-/*
-		EventQueue.invokeLater(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					FourConnectGUI g = new FourConnectGUI(player1, player2,
-							cols, rows);
-					JFrame f = new JFrame();
-					f.setSize(1000, 800);
-					f.setTitle("Connect 4");
-					f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					f.getContentPane().add(g);
-					f.setVisible(true);
-				} catch (Exception e) {
-					System.out
-							.println("There has been an error in the implementation of your at.connect4.game.GameLogic");
-				}
-			}
-		});
-*/
-		int winner = letPlay(player1,player2);
-		if(winner == player1.playerID){
-			System.out.println("Winner: Player 1");			
-		}else if(winner == player2.playerID){
-			System.out.println("Winner: Player 2");			
-		}else{
-			System.out.println("DRAW");
-		}
+	public static void write(String text, String filename){
+		try{
+			PrintWriter out = new PrintWriter(filename);
+			out.println(text);
+			out.flush();
+			out.close();
+		}catch(Exception e){}
 	}
 
-	
+	public static GameLogic getPCGameLogic(int depth, int pid){
+		GameLogic gl = new GameLogic();
+		gl.initializeGame(cols, rows, pid);
+		Evaluator e = new at.connect4.eval.PieceCounter(gl);
+		ISolver solver =  new at.connect4.solver.Negamax(gl.getPlayerID(), gl, depth, e);
+		gl.setSolver(solver);
+		return gl;
+	}
+
+	public static GameLogic getMCGameLogic(int depth, int pid, int nRuns){
+		GameLogic gl = new GameLogic();
+		gl.initializeGame(cols, rows, pid);
+		at.connect4.eval.MCEval e = new at.connect4.eval.MCEval(gl);
+		e.setRuns(nRuns);
+		ISolver solver = new at.connect4.solver.Negamax(gl.getPlayerID(), gl, depth, e);
+		gl.setSolver(solver);
+		return gl;
+	}
+	public static GameLogic getUCTGameLogic(int pid, int time){
+		GameLogic gl = new GameLogic();
+		gl.initializeGame(cols, rows, pid);
+		ISolver solver = new Uct4connect(gl.playerID, gl.opponentID, gl, time);
+		gl.setSolver(solver);
+		return gl;
+	}
+
+
+	public static void main(String[] arg) {
+		fullEval();
+	}
+	public static void fullEval(){
+
+		int[] runVal = {100,150,200};			
+		int[] searchTime = {1,2,3};		
+		int[] pcSearchDepths = {3,4,5,6};
+		int[] mcSearchDepths = {3,4};
+		StringBuffer sb = new StringBuffer();
+
+
+		int filecnt = -1;
+write(sb.toString(), ++filecnt +".txt");
+
+		/* Piece counter VS Piece Counter */
+ 		for(int depth1: pcSearchDepths){
+			for(int depth2: pcSearchDepths){
+				String s = 
+					"piece counter (" + depth1 + 
+					") VS piece counter (" + depth2 + "):" 
+					+ letPlay(getPCGameLogic(depth1, 1), getPCGameLogic(depth2, -1));
+				System.out.println(s);
+				sb.append(s + "\n");
+			}
+		}
+ 
+write(sb.toString(), ++filecnt +".txt");
+
+		/* mceval vs mceval */
+		for(int runs1: runVal){
+			for(int runs2: runVal){
+				for(int d1: mcSearchDepths){
+					for(int d2: mcSearchDepths){
+						String s = 
+							"mceval (" + d1 + 
+							", nruns " + runs1 + ") VS mceval (" + d2 + ", nruns " + runs2 + "):" 
+							+ letPlay(getMCGameLogic(d1, 1, runs1), getMCGameLogic(d2, -1, runs2));
+						System.out.println(s);
+						sb.append(s + "\n");
+					}
+				}
+			}
+		}
+write(sb.toString(), ++filecnt +".txt");
+		/* mceval  VS piece counter */
+		for(int nruns: runVal){
+			for(int d1: mcSearchDepths){
+				for(int d2: pcSearchDepths){
+					String s = 
+						"mceval (" + d1 +" nruns " + nruns + 
+						") VS piece counter (" + d2 + "):" 
+						+ letPlay(getMCGameLogic(d1, 1, nruns), getPCGameLogic(d2, -1));
+					System.out.println(s);
+					sb.append(s + "\n");
+					s = 
+						"piece counter (" + d2 + 
+						") VS mc eval (" + d1 + " nruns " + nruns + "):"  
+						+ letPlay(getPCGameLogic(d2, 1), getMCGameLogic(d1, -1, nruns));
+					System.out.println(s);
+					sb.append(s + "\n");
+				}
+			}
+		}
+		
+write(sb.toString(), ++filecnt +".txt");		
+		/* uct vs uct*/
+
+		for(int time1: searchTime){
+			for(int time2: searchTime){
+				String s = 
+						"uct (" +time1 + 
+						") VS uct (" + time2 + "):" 
+						+ letPlay(getUCTGameLogic(1, time1), getUCTGameLogic(-1, time2));
+					System.out.println(s);
+					sb.append(s + "\n");
+			}
+		}
+write(sb.toString(), ++filecnt +".txt");
+	/* uct vs mceval */
+		for(int time: searchTime){
+			for(int nruns: runVal){
+				for(int d: mcSearchDepths){
+					String s = 
+						"uct (" +time + ") "+
+						"VS" + 
+						"MCEval (" + d + "runs " + nruns + "): " 
+						+ letPlay(getUCTGameLogic(1, time),  getMCGameLogic(d, -1, nruns));
+					System.out.println(s);
+					sb.append(s + "\n");
+					 s = 
+						"MCEval (" + d + " runs " + nruns + "): " 						
+						+ "VS" 
+						+ "uct (" +time + ") "
+						+ letPlay( getMCGameLogic(d, 1, nruns), getUCTGameLogic(-1, time));
+
+					System.out.println(s);
+					sb.append(s + "\n");
+				}
+			}
+		}
+write(sb.toString(), ++filecnt +".txt");
+		/* uct vs piece counter  */
+		for(int time: searchTime){
+			for(int d: pcSearchDepths){
+				String s = 
+					"uct (" +time + ") "+
+					"VS" + 
+					"piece counter (" + d + "): " 			
+					+ letPlay(getUCTGameLogic(1, time),  getPCGameLogic(d, -1));
+				System.out.println(s);
+				sb.append(s + "\n");
+				 s = 
+					"piece counter (" + d + "): " 						
+					+ "VS" 
+					+ "uct (" +time + ") "
+					+ letPlay( getPCGameLogic(d, 1), getUCTGameLogic(-1, time));
+
+				System.out.println(s);
+				sb.append(s + "\n");
+			}
+		}
+
+write(sb.toString(), ++filecnt +".txt");		
+		System.out.println(sb.toString());
+
+	}
 
 	public static int letPlay(GameLogic player1, GameLogic player2){
 		int winner = 0;
@@ -132,8 +189,7 @@ public class ShowGame {
                 player1.insertCoin(col,player1.playerID);
                 winner = player1.gameFinished();
                 playerTurn = player2.playerID;                   
-            }
-            else{
+            }else{
                 col = player2.decideNextMove();               
                 player1.insertCoin(col,player2.playerID);
                 player2.insertCoin(col,player2.playerID);
